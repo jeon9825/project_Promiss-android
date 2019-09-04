26585dc1265bc20f27f7b8db39f1c4bf19025c23
jeon9825 <jeon9825@naver.com>
2019-09-04 19:03:53 +0900

    [android/jiyoon] modify login UI

--- app/src/main/java/com/skhu/cse/promiss/LoginActivity.java
+++ app/src/main/java/com/skhu/cse/promiss/LoginActivity.java
@@ -3,24 +3,48 @@ package com.skhu.cse.promiss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.skhu.cse.promiss.keyboard.SoftKeyboard;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText editText_id ;
     EditText editText_password;
+    InputMethodManager inputManager;
+    RelativeLayout loginView;
+    View center_view;
+    SoftKeyboard softKeyboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        center_view = findViewById(R.id.login_center_view);
+        inputManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        loginView=findViewById(R.id.login);
+        softKeyboard =new SoftKeyboard(loginView,inputManager);
+        softKeyboard.setSoftKeyboardCallback(new SoftKeyboard.SoftKeyboardChanged() {
+            @Override
+            public void onSoftKeyboardHide() {
 
+            }
+
+            @Override
+            public void onSoftKeyboardShow() {
+                center_view.setVisibility(View.GONE);
+            }
+        });
         editText_id=(EditText)findViewById(R.id.login_edit_id);
         editText_password=(EditText)findViewById(R.id.login_edit_password);
         Button b = (Button)findViewById(R.id.login_button);
+
         Intent i = getIntent();
         String id;
         String pw;
---
+++ app/src/main/java/com/skhu/cse/promiss/server/GetJson.java
@@ -0,0 +1,46 @@
+package com.skhu.cse.promiss.server;
+
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class GetJson {
+
+    private OkHttpClient client;
+    private static GetJson instance = new GetJson();
+
+    private String url = "http://106.10.54.206";
+    public static GetJson getInstance() {
+        return instance;
+    }
+
+    private GetJson() {
+        this.client = new OkHttpClient();
+    }
+
+    /**
+     * 웹 서버로 요청을 한다.
+     */
+    public void requestGet(String parameter, Callback callback) {
+
+        Request request = new Request.Builder()
+                .url(url) //통신하고자하는 url
+                .build();
+        client.newCall(request).enqueue(callback); //통신후 콜백될 함수
+    }
+
+    public void requestPost(String parameter, Callback callback) {
+
+        RequestBody body = new FormBody.Builder()
+
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url) //통신하고자하는 url
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(callback); //통신후 콜백될 함수
+    }
+}
