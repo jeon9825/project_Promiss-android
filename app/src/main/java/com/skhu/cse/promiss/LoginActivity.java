98a335d51e61cddd64b9f63858501d40a6d36a78
jeon9825 <jeon9825@naver.com>
2019-09-18 18:12:15 +0900

    [android/jiyoon] Complete LoginActivity

--- app/src/main/java/com/skhu/cse/promiss/LoginActivity.java
+++ app/src/main/java/com/skhu/cse/promiss/LoginActivity.java
@@ -30,25 +30,32 @@ import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText editText_id ;
+    EditText editText_id;
     EditText editText_password;
     InputMethodManager inputManager;
     RelativeLayout loginView;
     View center_view;
     SoftKeyboard softKeyboard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         center_view = findViewById(R.id.login_center_view);
         inputManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        loginView=findViewById(R.id.login);
-        softKeyboard =new SoftKeyboard(loginView,inputManager);
+        loginView = findViewById(R.id.login);
+        softKeyboard = new SoftKeyboard(loginView, inputManager);
         softKeyboard.setSoftKeyboardCallback(new SoftKeyboard.SoftKeyboardChanged() {
             @Override
             public void onSoftKeyboardHide() {
-
+                LoginActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        center_view.setVisibility(View.VISIBLE);
+                    }
+                });
             }
+
             @Override
             public void onSoftKeyboardShow() {
                 LoginActivity.this.runOnUiThread(new Runnable() {
@@ -59,27 +66,40 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
-        editText_id=(EditText)findViewById(R.id.login_edit_id);
-        editText_password=(EditText)findViewById(R.id.login_edit_password);
-        Button b = (Button)findViewById(R.id.login_button);
+        editText_id = (EditText) findViewById(R.id.login_edit_id);
+        editText_password = (EditText) findViewById(R.id.login_edit_password);
+        Button b = (Button) findViewById(R.id.login_button);
+        Button register_button = findViewById(R.id.register_button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String id = editText_id.getText().toString();
                 final String password = editText_password.getText().toString();
-                new Thread(){
+                if (id != null && password != null && id.equals("") && password.equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력하지않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                new Thread() {
                     @Override
                     public void run() {
                         super.run();
                         GetJson getJson = GetJson.getInstance();
-                        getJson.requestPost("api/User/Login",callback,"id",id,"pw",password);
+                        getJson.requestPost("api/User/Login", callback, "id", id, "pw", password);
                     }
-                };
+                }.run();
 //                Intent intent = new Intent(LoginActivity.this, MapActivity.class);
 //                startActivity(intent);
 //                finish();
             }
         });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+        );
     }
 
     private Callback callback = new Callback() {
@@ -89,7 +109,7 @@ public class LoginActivity extends AppCompatActivity {
             LoginActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(LoginActivity.this,"네트워크 연결 실패",Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "네트워크 연결 실패", Toast.LENGTH_LONG).show();
                 }
             });
         }
@@ -97,21 +117,21 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public void onResponse(Call call, Response response) throws IOException { // 통신 성공
             String result = response.body().string();
-            Log.d("server response:",result);
+            Log.d("server response:", result);
             try {
-                JSONObject object=new JSONObject(result);
+                JSONObject object = new JSONObject(result);
 
-                if(object.getInt("result")==2000){ //성공
+                if (object.getInt("result") == 2000) { //성공
 //                    =object.getString("data")
                     Intent intent = new Intent(LoginActivity.this, MapActivity.class);
                     startActivity(intent);
                     finish();
-                }else{ //실패
+                } else { //실패
                     final String message = object.getString("message");
                     LoginActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(LoginActivity.this,message,Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
                         }
                     });
                 }
--- app/src/main/java/com/skhu/cse/promiss/RegisterActivity.java
+++ app/src/main/java/com/skhu/cse/promiss/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -12,15 +13,27 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.skhu.cse.promiss.keyboard.SoftKeyboard;
+import com.skhu.cse.promiss.server.GetJson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText editText_ID;
     EditText editText_PW;
+    EditText editText_PW_check;
     InputMethodManager inputManager;
     RelativeLayout loginView;
     View center_view;
     SoftKeyboard softKeyboard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,23 +57,71 @@ public class RegisterActivity extends AppCompatActivity {
 //                });
 //            }
 //        });
-
-        editText_ID=findViewById(R.id.register_edit_id);
-        editText_PW=findViewById(R.id.register_edit_password);
+        editText_ID = findViewById(R.id.register_edit_id);
+        editText_PW = findViewById(R.id.register_edit_password);
+        editText_PW_check = findViewById(R.id.register_edit_password_check);
         Button button_OK = findViewById(R.id.register_button_ok);
         button_OK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String id = editText_ID.getText().toString();
                 final String pw = editText_PW.getText().toString();
-
-                Toast.makeText(RegisterActivity.this,"id는"+id+" 비밀번호는 "+pw,Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                intent.putExtra("id", id);
-                intent.putExtra("pw", pw);
-                startActivity(intent);
-                finish();
+                final String pw_check = editText_PW_check.getText().toString();
+                if (id != null && id.equals("")) {
+                    Toast.makeText(getApplicationContext(),"아이디를 입력하십시오.",Toast.LENGTH_LONG).show();
+                }
+                if(pw!=null&&pw.equals("")){
+                    Toast.makeText(getApplicationContext(),"비밀번호를 입력하십시오.",Toast.LENGTH_LONG).show();
+                }
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        GetJson getJson = GetJson.getInstance();
+                        getJson.requestPost("api/User/register", callback, "id", id, "pw", pw);
+                    }
+                }.run();
             }
         });
     }
+
+    private Callback callback = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) { // 통신 실패
+
+            RegisterActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(RegisterActivity.this, "네트워크 연결 실패", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException { // 통신 성공
+            String result = response.body().string();
+            Log.d("server response:", result);
+            try {
+                JSONObject object = new JSONObject(result);
+
+                if (object.getString("result").equals("OK")) { //성공
+//                    =object.getString("data")
+                    Intent intent = new Intent(RegisterActivity.this, MapActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else { //실패
+                    final String message = object.getString("message");
+                    RegisterActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            // JSONArray array;
+        }
+    };
 }
