e0d781cb2d4fd67a547f2738205ee23a47d48171
jeon9825 <jeon9825@naver.com>
2019-09-18 21:03:57 +0900

    [android/jiyoon] Complete RegisterActivity

--- app/src/main/java/com/skhu/cse/promiss/RegisterActivity.java
+++ app/src/main/java/com/skhu/cse/promiss/RegisterActivity.java
@@ -2,14 +2,18 @@ package com.skhu.cse.promiss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.skhu.cse.promiss.keyboard.SoftKeyboard;
@@ -29,6 +33,8 @@ public class RegisterActivity extends AppCompatActivity {
     EditText editText_ID;
     EditText editText_PW;
     EditText editText_PW_check;
+    TextView textView_ID_check;
+    TextView textView_PW_check;
     InputMethodManager inputManager;
     RelativeLayout loginView;
     View center_view;
@@ -58,8 +64,11 @@ public class RegisterActivity extends AppCompatActivity {
 //            }
 //        });
         editText_ID = findViewById(R.id.register_edit_id);
+        textView_ID_check = findViewById(R.id.register_id_check);
+        textView_ID_check.setVisibility(View.GONE);  // 이미 존재하는 ID입니다. GONE
         editText_PW = findViewById(R.id.register_edit_password);
         editText_PW_check = findViewById(R.id.register_edit_password_check);
+        textView_PW_check = findViewById(R.id.register_pw_check);
         Button button_OK = findViewById(R.id.register_button_ok);
         button_OK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +90,26 @@ public class RegisterActivity extends AppCompatActivity {
                         getJson.requestPost("api/User/register", callback, "id", id, "pw", pw);
                     }
                 }.run();
+            }
+        });
+        editText_PW_check.addTextChangedListener(new TextWatcher() { //키보드 입력받으면서 비밀번호 확인
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editText_PW_check.getText().toString().equals(editText_PW.getText().toString())){
+                    textView_PW_check.setText("일치 ");
+                    textView_PW_check.setTextColor(getResources().getColor(R.color.same));
+                }
             }
         });
     }
@@ -111,12 +140,14 @@ public class RegisterActivity extends AppCompatActivity {
                     finish();
                 } else { //실패
                     final String message = object.getString("message");
-                    RegisterActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    if(message.equals("ID중복")){
+                        RegisterActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView_ID_check.setVisibility(View.VISIBLE); //외부 스레드에서 UI 변경할때
+                            }
+                        });
+                    }
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
